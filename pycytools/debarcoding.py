bf6ecf760c3d0f330fd7a95adf3fd8ec4d205f52
votti <vito.zanotelli@gmail.com>
2017-05-22 17:01:35 +0000

    fixes bugs and restructures the debarcoding

--- pycytools/debarcoding.py
+++ pycytools/debarcoding.py
@@ -3,8 +3,14 @@
 Contains various functions for debarcoding
 @author: mleutenegger
 """
+import pandas as pd
+import numpy as np
 
-def debarcode(plot_cells, bc_key, dist = 30):
+NAME_BARCODE = 'barcode'
+NAME_INVALID = 'invalid'
+NAME_WELLCOLUMN = 'well'
+
+def debarcode(data, bc_key, dist = 30):
     """
     Debarcode a Dataframe from the image data.
 
@@ -20,13 +26,45 @@ def debarcode(plot_cells, bc_key, dist = 30):
         dictionary {imageNumber => well}
     """
 
-    # prepare a dicitionary containing the barcode
-    tab_bc = bc_key.set_index('well')
-    order = tab_bc.columns
-    tab_bc['barcode'] = tab_bc.apply(lambda x: ''.join([str(int(v)) for v in x]),axis=1)
-    tab_bc = tab_bc.reset_index(drop=False)
-    bc_dict = {row['barcode']: row['well'] for idx, row in tab_bc.iterrows()}
+    data = data.copy()
+    data = _treshold_data(data)
+    data = _debarcode_data(bc_key, data)
+    bc_dic = _summarize_singlecell_barcodes(data)
+    data = data.drop('present', axis=1)
+    
+    return bc_dic, data
 
+def _treshold_data(bc_dat):
+    bc_dat = bc_dat.copy()
+    bc_dat = bc_dat.apply(lambda x: (x-np.mean(x))/np.std(x),)
+    bc_dat[bc_dat > 0] = 1
+    bc_dat[bc_dat < 0] = 0
+    return bc_dat
+
+def _debarcode_data(bc_key, bc_dat):
+    tab_bc = bc_key.set_index(NAME_WELLCOLUMN)
+    metals = list(tab_bc.columns)
+    tab_bc[NAME_BARCODE] = tab_bc.apply(lambda x: ''.join([str(int(v)) for v in x]),axis=1)
+    tab_bc = tab_bc.reset_index(drop=False)
+    bc_dict = {row[NAME_BARCODE]: row[NAME_WELLCOLUMN] for idx, row in tab_bc.iterrows()}
+
+
+    data = bc_dat.loc[:, metals].copy()
+    data = data.rename(columns={chan: ''.join(c for c in chan if c.isdigit()) for chan in data.columns})
+    data[NAME_BARCODE] = data.apply(lambda x: ''.join([str(int(v)) for v in x]),axis=1)
+    data[NAME_WELLCOLUMN] = [bc_dict.get(b, NAME_INVALID) for b in data[NAME_BARCODE]]
+
+    data= data.set_index(NAME_WELLCOLUMN, append=True)
+    data= data.set_index(NAME_BARCODE, append=True)
+
+    data['count'] = 1
+
+    data = data.loc[data.index.get_level_values(NAME_WELLCOLUMN) != '' ,'count']
+    data = data.reset_index(drop=False, level=[NAME_WELLCOLUMN, NAME_BARCODE], name='present')
+    return data
+
+def _summarize_singlecell_barcodes(data):
+    # prepare a dicitionary containing the barcode
     dic = pd.DataFrame(columns=[
         'ImageNumber',
         'well',
@@ -35,37 +73,16 @@ def debarcode(plot_cells, bc_key, dist = 30):
         'highest_bc_count',
         'second_bc_count'
     ])
-    amb_fil = plot_cells['filter','is-ambiguous'] == False
-
-    data = plot_cells['MeanIntensity'].loc[(plot_cells[('MeanIntensity','dist-rim')] < dist) & amb_fil, :]
-    data = data.loc[:, [c for c in data.columns if (any(m in c for m in metals))]]
-    data = data.apply(lambda x: (x-np.mean(x))/np.std(x),)
-    data[data > 0] = 1
-    data[data < 0] = 0
-    data = data.rename(columns={chan: ''.join(c for c in chan if c.isdigit()) for chan in data.columns})
-    data.columns = data.columns.map(lambda x: int(x))
-    data.reindex_axis(sorted(data), axis=1)
-    data.columns = data.columns.map(lambda x: str(x))
-    data['barcode'] = data[bcmass].apply(lambda x: ''.join([str(int(v)) for v in x]),axis=1)
-    data['well'] = [bc_dict.get(b, 'invalid') for b in data['barcode']]
-
-    data= data.set_index('well', append=True)
-    data= data.set_index('barcode', append=True)
-
-    data['count'] = 1
-
-    data = data.loc[data.index.get_level_values('well') != '' ,'count']
-    data = data.reset_index(drop=False, level=['well', 'barcode'], name='present')
 
     for imagenr in data.index.get_level_values('ImageNumber').unique():
         #TODO: catch indexError and propose increase of range.
         temp = {
             'ImageNumber':int(imagenr)
         }
-        summary = data.xs(imagenr, level='ImageNumber')['well'].value_counts()
+        summary = data.xs(imagenr, level='ImageNumber')[NAME_WELLCOLUMN].value_counts()
         try:
-            temp['invalid_bcs']=summary['invalid']
-            del(summary['invalid'])
+            temp['invalid_bcs']=summary[NAME_INVALID]
+            del(summary[NAME_INVALID])
         except KeyError:
             temp['invalid_bcs']=0
         temp['well']=summary.keys()[0]
@@ -74,8 +91,7 @@ def debarcode(plot_cells, bc_key, dist = 30):
         if len(summary) > 1:
             temp['second_bc_count']=summary[summary.keys()[1]]
         else:
-            temp['second_bc_count']='0'
+            temp['second_bc_count']=0
         dic.loc[len(dic)]= temp
-
-    dic['ImageNumber'] = dic['ImageNumber'].apply(lambda x: int(x))
     return dic
+
