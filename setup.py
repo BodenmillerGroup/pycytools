a6d9fbb27d41dbf87a21cdb95926605e02e1ee7b
votti <mcvotti@gmail.com>
2020-09-25 16:14:34 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -6,12 +6,9 @@ from setuptools import setup, find_packages
 with open('README.rst') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
-    license = f.read()
-
 setup(
     name='pycytools',
-    version='0.6.7',
+    version='0.6.9',
     description='Helper functions to handle image cytometry data.',
     long_description=readme,
     long_description_content_type='text/x-rst',
