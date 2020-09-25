86f8e20caa8a50a73ec97c4eb815eede050da97e
votti <mcvotti@gmail.com>
2020-09-25 10:28:54 +0200

    Add comments

--- pycytools/transformations.py
+++ pycytools/transformations.py
@@ -1,21 +1,29 @@
 import numpy as np
 
 
-def logtransf_data(x):
+def logtransf_data(x, offset=None):
     """
+    Log transformation with a small value added
 
-    :param x:
-    :return:
+    To deal with 0's, either add a small offset manually
+    or add the minimal non zero value.
+
+    :param x: values
+    :param offset: An offset, set to 0 for no offset.
+    :return: Transformed values
     """
-    xmin = min(x[x > 0])
-    return (np.log10(x + xmin))
+    if offset is None:
+        offset = min(x[x > 0])
+    return (np.log10(x + offset))
 
 
 def asinhtransf_data(x, cof=5):
     """
+    Asinh transformation accoring to
+    asinh(x/cof)
 
-    :param x:
-    :param cof:
-    :return:
+    :param x: values
+    :param cof: cofactor
+    :return: transformed values
     """
     return np.arcsinh(x / cof)
