14eb8d508eb4925940de453a9ad718d115401e85
votti <mcvotti@gmail.com>
2020-09-25 11:03:54 +0200

    Fix url

--- setup.py
+++ setup.py
@@ -11,13 +11,13 @@ with open('LICENSE') as f:
 
 setup(
     name='pycytools',
-    version='0.6.6',
+    version='0.6.7',
     description='Helper functions to handle image cytometry data.',
     long_description=readme,
     long_description_content_type='text/x-rst',
     author='Vito Zanotelli, Matthias Leutenegger, Bernd Bodenmiller',
     author_email='vito.zanotelli@gmail.com',
-    url='https://github.com/bodenmillerlab/pycytools',
+    url='https://github.com/BodenmillerGroup/pycytools',
     license='MIT License',
     packages=find_packages(exclude=('tests', 'docs')),
     install_requires = ['numpy',
